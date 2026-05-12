7be0a55363d6a7fd294cf17b1ddfa0e2ca9e9651
adamt <AdamKhachikov@yandex.ru>
2026-05-12 21:10:05 +0300

    Правдивые курсы + фикс багов

--- src/data/db_session.py
+++ src/data/db_session.py
@@ -2,8 +2,6 @@ import sqlalchemy as sa
 import sqlalchemy.orm as orm
 from sqlalchemy.orm import Session
 
-
-
 SqlAlchemyBase = orm.declarative_base()
 
 __factory = None
@@ -15,14 +13,18 @@ def global_init(db_file):
         return
 
     if not db_file or not db_file.strip():
-        raise Exception("db")
+        raise Exception("db/users.db")
 
     conn_str = f'sqlite:///{db_file.strip()}?check_same_thread=False'
     print(f"Подключение к базе данных по адресу {conn_str}")
 
     engine = sa.create_engine(conn_str, echo=False)
     __factory = orm.sessionmaker(bind=engine)
-        
+
+    from . import __all_models
+
+    SqlAlchemyBase.metadata.create_all(engine)
+
 def create_session() -> Session:
     global __factory
     return __factory()
